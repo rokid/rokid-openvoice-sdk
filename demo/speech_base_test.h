4a49973f19d594707545e5ffa111e4c8cbcb146f
chen.zhang <chen.zhang@rokid.com>
2019-03-15 14:04:00 +0800

    retab

--- demo/speech_base_test.h
+++ demo/speech_base_test.h
@@ -5,16 +5,16 @@
 
 class SpeechBaseTest {
 public:
-	static void test(const rokid::speech::PrepareOptions& opts, const DemoOptions& dopts);
+  static void test(const rokid::speech::PrepareOptions& opts, const DemoOptions& dopts);
 
-	void run(const rokid::speech::PrepareOptions& opt, const uint8_t* data, uint32_t size);
+  void run(const rokid::speech::PrepareOptions& opt, const uint8_t* data, uint32_t size);
 
 protected:
-	void speech_poll_routine(std::shared_ptr<rokid::speech::Speech>& speech);
+  void speech_poll_routine(std::shared_ptr<rokid::speech::Speech>& speech);
 
-  void do_request(uint32_t off, const uint8_t* data, uint32_t size,
+  uint32_t do_request(uint32_t off, const uint8_t* data, uint32_t size,
                   std::shared_ptr<rokid::speech::Speech> &speech);
 
 private:
-	bool requiring = false;
+  bool requiring = false;
 };
