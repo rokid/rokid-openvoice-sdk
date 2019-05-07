7eb68071515eab18a6cdee0cdd043b04c2f160dc
ming <chen.zhang@rokid.com>
2019-05-07 13:11:28 +0800

    vsys compile
    add phony-speech module
    set vad mode from java api

--- vsys.mk
+++ vsys.mk
@@ -10,3 +10,8 @@ include $(clear-vars)
 local.module := rokid_speech
 local.src-paths := android/java
 include $(build-java-lib)
+
+include $(clear-vars)
+local.module := phony-speech
+local.ndk-modules := speech
+include $(build-executable)
