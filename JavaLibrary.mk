d9a4ea9ce8792bf0942abc259444b14731ccc280
qiuren.fan <qiuren.fan@rokid.com>
2019-07-03 15:53:22 +0800

    compile on android8.1

--- JavaLibrary.mk
+++ JavaLibrary.mk
@@ -10,17 +10,7 @@ LOCAL_C_INCLUDES := \
 	$(LOCAL_PATH)/include \
 	$(LOCAL_PATH)/src/common
 LOCAL_CPPFLAGS := $(COMMON_FLAGS) -std=c++11
-LOCAL_SHARED_LIBRARIES := libnativehelper libspeech liblog
-ifeq ($(PLATFORM_SDK_VERSION), 22)
-LOCAL_SHARED_LIBRARIES += libc++ libdl
-LOCAL_C_INCLUDES += external/libcxx/include
-else ifeq ($(PLATFORM_SDK_VERSION), 19)
-LOCAL_SDK_VERSION := 14
-LOCAL_NDK_STL_VARIANT := gnustl_static
-LOCAL_C_INCLUDES += libnativehelper/include/nativehelper
-else
-LOCAL_CXX_STL := libc++
-endif
+LOCAL_SHARED_LIBRARIES := libnativehelper libspeech librlog
 LOCAL_SRC_FILES := \
 		$(JNI_DIR)/com_rokid_speech_Tts.cc \
 		$(JNI_DIR)/com_rokid_speech_Speech.cc \
@@ -41,49 +31,13 @@ LOCAL_SRC_FILES := \
 include $(BUILD_STATIC_JAVA_LIBRARY)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE_TAGS:=optional
-LOCAL_PROGUARD_ENABLED := disabled
-LOCAL_PACKAGE_NAME := RKSpeechDemo
-LOCAL_CERTIFICATE := platform
-LOCAL_SRC_FILES := \
-		$(EXAMPLE_JAVA_DIR)/com/rokid/speech/example/TtsDemo.java \
-		$(EXAMPLE_JAVA_DIR)/com/rokid/speech/example/TtsPlayerDemo.java \
-		$(EXAMPLE_JAVA_DIR)/com/rokid/speech/example/SpeechDemo.java
-LOCAL_STATIC_JAVA_LIBRARIES := rokid_speech
-include $(BUILD_PACKAGE)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := tts_sdk.json
-LOCAL_MODULE_CLASS := ETC
-LOCAL_MODULE_PATH := $(TARGET_OUT)/etc
-LOCAL_SRC_FILES := android/etc/tts_sdk.json
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := speech_sdk.json
-LOCAL_MODULE_CLASS := ETC
-LOCAL_MODULE_PATH := $(TARGET_OUT)/etc
-LOCAL_SRC_FILES := android/etc/speech_sdk.json
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE := librokid_opus_jni
 LOCAL_CPP_EXTENSION := .cc
 LOCAL_CPPFLAGS := $(COMMON_FLAGS) -std=c++11
-LOCAL_SHARED_LIBRARIES := libnativehelper liblog libspeech librkcodec
+LOCAL_SHARED_LIBRARIES := libnativehelper librlog libspeech librkcodec
 LOCAL_C_INCLUDES := \
 	external/libopus/include \
 	$(LOCAL_PATH)/src/common
-ifeq ($(PLATFORM_SDK_VERSION), 22)
-LOCAL_SHARED_LIBRARIES += libc++ libdl
-LOCAL_C_INCLUDES += external/libcxx/include
-else ifeq ($(PLATFORM_SDK_VERSION), 19)
-LOCAL_SDK_VERSION := 14
-LOCAL_NDK_STL_VARIANT := gnustl_static
-LOCAL_C_INCLUDES += libnativehelper/include/nativehelper
-else
-LOCAL_CXX_STL := libc++
-endif
 LOCAL_SRC_FILES := \
 		$(JNI_DIR)/com_rokid_speech_Opus.cc
 include $(BUILD_SHARED_LIBRARY)
--- core.mk
+++ core.mk
@@ -19,24 +19,18 @@ LOCAL_C_INCLUDES := \
 	external/zlib
 
 COMMON_SRC := \
-	src/common/rlog.c \
-	src/common/rlog.h \
 	src/common/speech_connection.cc \
-	src/common/speech_connection.h \
 	src/common/nanopb_encoder.cc \
-	src/common/nanopb_encoder.h \
 	src/common/nanopb_decoder.cc \
-	src/common/nanopb_decoder.h
+	src/common/alt_chrono.cc \
+	src/common/log_plugin.cc \
+	src/common/trace-uploader.cc
 
 TTS_SRC := \
-	src/tts/tts_impl.cc \
-	src/tts/tts_impl.h \
-	src/tts/types.h
+	src/tts/tts_impl.cc
 
 SPEECH_SRC := \
-	src/speech/speech_impl.cc \
-	src/speech/speech_impl.h \
-	src/speech/types.h
+	src/speech/speech_impl.cc
 
 NANOPB_SRC := \
 	nanopb/pb_common.c \
@@ -51,20 +45,8 @@ LOCAL_SRC_FILES := \
 	$(NANOPB_SRC)
 
 LOCAL_CPPFLAGS := $(COMMON_CFLAGS) -std=c++11
-LOCAL_SHARED_LIBRARIES := liblog libuWS libcrypto libz
+LOCAL_SHARED_LIBRARIES := librlog libuWS libcrypto libz
 LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)/include
-
-ifeq ($(PLATFORM_SDK_VERSION), 22)
-LOCAL_SHARED_LIBRARIES += libc++ libdl
-LOCAL_C_INCLUDES += external/libcxx/include
-else ifeq ($(PLATFORM_SDK_VERSION), 19)
-LOCAL_SDK_VERSION := 14
-LOCAL_NDK_STL_VARIANT := gnustl_static
-LOCAL_CPPFLAGS := -D__STDC_FORMAT_MACROS
-else
-LOCAL_CXX_STL := libc++
-endif
-
 include $(BUILD_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
@@ -72,47 +54,11 @@ LOCAL_MODULE := librkcodec
 LOCAL_MODULE_TAGS := optional
 LOCAL_CPP_EXTENSION := .cc
 LOCAL_SRC_FILES := \
-	src/codec/rkcodec.cc \
-	include/rkcodec.h
+	src/codec/rkcodec.cc
 LOCAL_C_INCLUDES := \
 	$(LOCAL_PATH)/include \
 	$(LOCAL_PATH)/src/common \
 	external/libopus/include
-LOCAL_SHARED_LIBRARIES := libopus libspeech
+LOCAL_SHARED_LIBRARIES := libopus libspeech librlog
 LOCAL_CPPFLAGS := -std=c++11
-ifeq ($(PLATFORM_SDK_VERSION), 22)
-LOCAL_SHARED_LIBRARIES += libc++ libdl
-else ifeq ($(PLATFORM_SDK_VERSION), 19)
-LOCAL_SDK_VERSION := 14
-LOCAL_NDK_STL_VARIANT := gnustl_static
-else
-LOCAL_CXX_STL := libc++
-endif
 include $(BUILD_SHARED_LIBRARY)
-
-# module speech_demo
-include $(CLEAR_VARS)
-LOCAL_MODULE := speech_demo
-LOCAL_MODULE_TAGS := optional
-LOCAL_CPP_EXTENSION := .cc
-LOCAL_SRC_FILES := \
-	demo/demo.cc \
-	demo/simple_wave.cc \
-	demo/speech_stress_test.cc
-LOCAL_C_INCLUDES := \
-	$(PROTOC_OUT_DIR) \
-	$(LOCAL_PATH)/include \
-	$(LOCAL_PATH)/src/common \
-	external/boringssl/include
-LOCAL_SHARED_LIBRARIES := libspeech
-LOCAL_CPPFLAGS := $(COMMON_CFLAGS) \
-	-std=c++11
-ifeq ($(PLATFORM_SDK_VERSION), 22)
-LOCAL_SHARED_LIBRARIES += libc++ libdl
-else ifeq ($(PLATFORM_SDK_VERSION), 19)
-LOCAL_SDK_VERSION := 14
-LOCAL_NDK_STL_VARIANT := gnustl_static
-else
-LOCAL_CXX_STL := libc++
-endif
-include $(BUILD_EXECUTABLE)
